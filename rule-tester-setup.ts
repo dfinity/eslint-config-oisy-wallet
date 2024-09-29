ce471825943a95d15d6f8b08f5c28e9640145dae
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-09-29 13:49:21 +0200

    refactor: remove wildcard

--- rule-tester-setup.ts
+++ rule-tester-setup.ts
@@ -1,9 +1,9 @@
 import { RuleTester } from "@typescript-eslint/rule-tester";
-import * as vitest from "vitest";
+import { afterAll, it, describe } from "vitest";
 
 // Assign vitest testing methods to RuleTester
 // https://typescript-eslint.io/packages/rule-tester#vitest
-RuleTester.afterAll = vitest.afterAll;
-RuleTester.it = vitest.it;
-RuleTester.itOnly = vitest.it.only;
-RuleTester.describe = vitest.describe;
+RuleTester.afterAll = afterAll;
+RuleTester.it = it;
+RuleTester.itOnly = it.only;
+RuleTester.describe = describe;
