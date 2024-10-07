e8c762ce8b24508a7efdde91cf860c6ff13c9d4d
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-10-07 08:09:02 +0200

    feat: avoid relative imports (#17)
    * feat: avoid relative imports
    
    * chore: remove unused imports
    
    * fix: wrong schema
    
    ---------
    
    Co-authored-by: David Dal Busco <david.dalbusco@outlook.com>

--- eslint-local-rules.cjs
+++ eslint-local-rules.cjs
@@ -217,4 +217,33 @@ module.exports = {
       };
     },
   },
+  "no-relative-imports": {
+    meta: {
+      type: "suggestion",
+      docs: {
+        description: "Disallow all relative imports",
+        category: "Best Practices",
+        recommended: false,
+      },
+      messages: {
+        noRelativeImports:
+          "Relative imports are not allowed. Use an alias or absolute imports.",
+      },
+      schema: [],
+    },
+    create(context) {
+      return {
+        ImportDeclaration(node) {
+          const path = node.source.value;
+
+          if (path.startsWith("./")) {
+            context.report({
+              node,
+              messageId: "noRelativeImports",
+            });
+          }
+        },
+      };
+    },
+  },
 };
---
+++ tests/no-relative-imports.spec.ts
@@ -0,0 +1,27 @@
+import { RuleTester } from "@typescript-eslint/rule-tester";
+
+const rule = require("../eslint-local-rules.cjs")["no-relative-imports"];
+
+const ruleTester = new RuleTester();
+
+ruleTester.run("no-relative-imports", rule, {
+  valid: [
+    {
+      code: 'import moduleA from "moduleA";',
+    },
+    {
+      code: 'import { something } from "@alias/something";',
+    },
+  ],
+
+  invalid: [
+    {
+      code: 'import moduleB from "./moduleB";',
+      errors: [{ messageId: "noRelativeImports" }],
+    },
+    {
+      code: 'import moduleC from "./path/moduleC";',
+      errors: [{ messageId: "noRelativeImports" }],
+    },
+  ],
+});
