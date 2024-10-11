e0d615d51ba4102a210428dd7c560d65ee7e3f13
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-10-11 19:28:29 +0200

    feat: add new rule explicit-non-void-return-type (#23)

--- eslint-local-rules.cjs
+++ eslint-local-rules.cjs
@@ -3,4 +3,5 @@ module.exports = {
   "use-option-type-wrapper": require("./rules/use-option-type-wrapper"),
   "prefer-object-params": require("./rules/prefer-object-params"),
   "no-relative-imports": require("./rules/no-relative-imports"),
+  "explicit-non-void-return-type": require("./rules/explicit-non-void-return-type"),
 };
