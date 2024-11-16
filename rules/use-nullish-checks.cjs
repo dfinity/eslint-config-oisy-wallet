bb0c8b1ccaf2becc777295aa78429e4c9385ea20
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-16 01:11:01 +0100

    feat: add option to ignore find in nullish rule

--- rules/use-nullish-checks.cjs
+++ rules/use-nullish-checks.cjs
@@ -13,19 +13,48 @@ module.exports = {
         "Use nonNullish() instead of direct variable check for nullish checks.",
     },
     fixable: "code",
-    schema: [],
+    schema: [
+      {
+        type: "object",
+        properties: {
+          allowFindUndefinedCheck: {
+            type: "boolean",
+            default: false,
+          },
+        },
+        additionalProperties: false,
+      },
+    ],
   },
 
   create(context) {
+    const options = context.options[0] || {};
+    const ignoreFind = options.allowFindUndefinedCheck || false;
+
     const binaryCheck = (node) => {
       if (node.type === "BinaryExpression") {
-        return (
+        const isNullishCheck =
           (node.operator === "===" || node.operator === "!==") &&
           ((node.right.type === "Identifier" &&
             node.right.name === "undefined") ||
             // eslint-disable-next-line local-rules/use-nullish-checks -- This is the statement to check if the right side is null, not undefined
-            (node.right.type === "Literal" && node.right.value === null))
-        );
+            (node.right.type === "Literal" && node.right.value === null));
+
+        if (ignoreFind) {
+          const isFind =
+              node.left &&
+              node.left.type === "CallExpression" &&
+              node.left.callee &&
+              node.left.callee.type === "MemberExpression" &&
+              node.left.callee.property &&
+              node.left.callee.property.name === "find";
+
+          if (isFind) {
+            return false;
+          }
+        }
+
+        return isNullishCheck;
       }
     };
 
