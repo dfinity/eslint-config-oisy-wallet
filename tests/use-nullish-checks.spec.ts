f9303d7a92894fa8e95321ac7fb26a06f5b82fca
Antonio Ventilii <antonioventilii@gmail.com>
2026-03-30 11:10:56 +0200

    test: Add Typescript file for type-safe tests (#222)
    # Motivation
    
    For rule use-nullish-checks, we run the tests in a Typescript file since
    the rule actually will rely on Typescript to perform some checks (see PR
    https://github.com/dfinity/eslint-config-oisy-wallet/pull/221).

--- tests/use-nullish-checks.spec.ts
+++ tests/use-nullish-checks.spec.ts
@@ -1,53 +1,73 @@
 import { RuleTester } from "@typescript-eslint/rule-tester";
+import * as path from "path";
 const rule = require("../rules/use-nullish-checks.cjs");
 
-const ruleTester = new RuleTester();
+const ruleTester = new RuleTester({
+  languageOptions: {
+    parser: require("@typescript-eslint/parser"),
+  },
+});
+
+const filename = path.join(__dirname, "..", "rule-test-file.ts");
 
 ruleTester.run("use-nullish-checks", rule, {
   valid: [
     {
       code: "isNullish(foo);",
+      filename,
     },
     {
       code: "nonNullish(bar);",
+      filename,
     },
     {
       code: "if (a === b) {}",
+      filename,
     },
     {
       code: "if (a !== b) {}",
+      filename,
     },
     {
       code: "if (a > b) {}",
+      filename,
     },
     {
       code: "if (a < b) {}",
+      filename,
     },
     {
       code: "if (!nonNullish(foo)) {}",
+      filename,
     },
     {
       code: "if (isNullish(foo)) {}",
+      filename,
     },
     {
       code: "const b: boolean = true; if (b) {}",
+      filename,
     },
     {
       code: "const b: boolean | undefined = undefined; if (b) {}",
+      filename,
     },
     {
       code: "const foo: boolean | null | undefined = null; if (foo) {}",
+      filename,
     },
   ],
 
   invalid: [
     {
       code: "foo === null;",
+      filename,
       errors: [{ messageId: "isNullish" }],
       output: "isNullish(foo);",
     },
     {
       code: "foo !== undefined;",
+      filename,
       errors: [{ messageId: "nonNullish" }],
       output: "nonNullish(foo);",
     },
